ca0b74a0747fec502e56ee4c0c38887c548baa76
Jerry X <linghui10@foxmail.com>
2024-04-03 17:28:45 +0200

    add hip assistance

---
+++ xlh_test_dynamicEquation.m
@@ -0,0 +1,7 @@
+
+pathOpenSimModel = 'C:\Users\lingh\OneDrive - KTH\ExMaterials\7-Doctor\Research\2-simulation\Subjects\Falisse_et_al_2022\Falisse_et_al_2022.osim';
+outputDir = 'C:\Users\lingh\OneDrive - KTH\ExMaterials\7-Doctor\Research\2-simulation\Subjects\Falisse_et_al_2022';
+outputFilename =  'F_Falisse_et_al_2022';
+verbose_mode = 1;
+
+VerifyInverseDynamics(pathOpenSimModel, outputDir, outputFilename, verbose_mode)
